fb9fb6b9a3badcfd7d7064358888728733e88ac9
admin <aassdd80915@gmail.com>
2025-11-30 17:54:30 +0800

    policy頁面

--- src/views/Policy.vue
+++ src/views/Policy.vue
@@ -1,5 +1,14 @@
-<template>
+<script setup lang="ts">
+import MemberOrders from '@/components/memberOrders.vue';
+import MemberPagebutton from '@/components/memberPagebutton.vue';
+import Pagelinebar from '@/components/Pagelinebar.vue';
 
+</script>
+
+<template>
+  <MemberPagebutton />
+  <Pagelinebar />
+  <MemberOrders />
 </template>
 
 <style>
