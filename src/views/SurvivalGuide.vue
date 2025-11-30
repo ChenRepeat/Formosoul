f9705e02da8b67606907c09b49d30b6c5712cd24
Timing <taimingghuang@gmail.com>
2025-11-30 16:58:08 +0800

    123

--- src/views/SurvivalGuide.vue
+++ src/views/SurvivalGuide.vue
@@ -1,6 +1,16 @@
 <template>
 
+<ProductCard /> <!-- 引用 -->
+
+
 </template>
+
+<script setup>
+  import ProductCard from '@/components/ProductCard.vue' // 引用
+
+
+</script>
+
 
 <style>
 @media (min-width: 1024px) {
