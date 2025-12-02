a98dc25c8bccf0b19df0f90f5d955db05b3b05e6
Timing <taimingghuang@gmail.com>
2025-12-02 14:54:51 +0800

    add

--- src/views/SurvivalGuide.vue
+++ src/views/SurvivalGuide.vue
@@ -1,29 +1,28 @@
 <!-- <ProductCard /> 引用 -->
 <script setup>
-  import ProductCard from '@/components/ProductCard.vue' // 引用
-
+import ProductCard from "@/components/ProductCard.vue"; // 引用
 </script>
 
-
 <template>
-<!-- :style="{top: `${position.y}px`, left: `${position.x}px`}" -->
+  <!-- :style="{top: `${position.y}px`, left: `${position.x}px`}" -->
   <main class="survival-case">
-    <div ref="lanternRef" class="survial-lantern-case" >
-      <img src="/public/SurvivalGuide/lantern-image.png" class="survial-lantern-img-case" alt="">
+    <div ref="lanternRef" class="survial-lantern-case">
+      <img
+        src="/public/SurvivalGuide/lantern-image.png"
+        class="survial-lantern-img-case"
+        alt=""
+      />
     </div>
 
     <section>
       <div class="survival-headline-case">
-        <h3 class="survival-headline-inner1-case">Welcome to survival guide！</h3>
-
-
+        <h3 class="survival-headline-inner1-case">
+          Welcome to survival guide！
+        </h3>
       </div>
     </section>
   </main>
-
-
 </template>
-
 
 <style scoped lang="scss">
 @media (min-width: 1024px) {
@@ -38,7 +37,4 @@
   // color: $color-primary;
   font-weight: bold;
 }
-
-
-
 </style>
