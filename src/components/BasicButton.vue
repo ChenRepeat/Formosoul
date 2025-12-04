4e0ae82176a53fc1e5f815c0fcf5a5d3441f1813
michael9487 <michael4292015@gmail.com>
2025-12-04 13:25:04 +0800

    add none

--- src/components/BasicButton.vue
+++ src/components/BasicButton.vue
@@ -1,23 +1,22 @@
 <script setup>
-  //  button 事件使用方法
-  //  在父組件定義事件
-  //  import { ref } from 'vue';
-  //  import Button from './Button.vue';
+//  button 事件使用方法
+//  在父組件定義事件
+//  import { ref } from 'vue';
+//  import Button from './Button.vue';
 
+//  範例如下
 
-  //  範例如下
+//  要使用到事件切換的話，class名在這邊定義
+//  const currentClass = ref('btn');
 
-  //  要使用到事件切換的話，class名在這邊定義
-  //  const currentClass = ref('btn'); 
-
-  //  切換 class 實例
-  //  const ch = () => {
-  //    if (currentClass.value === 'btn') {
-  //       currentClass.value = 'btn-a'; 
-  //    } else {
-  //       currentClass.value = 'btn';
-  //    }
-  //  };
+//  切換 class 實例
+//  const ch = () => {
+//    if (currentClass.value === 'btn') {
+//       currentClass.value = 'btn-a';
+//    } else {
+//       currentClass.value = 'btn';
+//    }
+//  };
 </script>
 
 <template>
@@ -37,17 +36,17 @@
     -->
 </template>
 
-<style scope>
-  .btn{
-    background-color: red;
-  }
-  .btn-a{
-    background-color: green;
-  }
-  .btn:hover{
-    background-color: yellow;
-  }
-  .btn-a:hover{
-    background-color: blue;
-  }
+<style scoped>
+.btn {
+  background-color: red;
+}
+.btn-a {
+  background-color: green;
+}
+.btn:hover {
+  background-color: yellow;
+}
+.btn-a:hover {
+  background-color: blue;
+}
 </style>
