78c9e60354c839896b0a4b4014b095fa14922bb8
irenewhc <wh410601@gmail.com>
2025-12-21 12:42:35 +0800

    add ProductList 收藏功能點擊切換+阻止冒泡

--- src/components/SHOP/ProductList.vue
+++ src/components/SHOP/ProductList.vue
@@ -2,14 +2,19 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { useRouter } from "vue-router";     //使用路由功能
 import TestProductCard from '@/components/TestProductCard.vue';
+import { ref } from 'vue';
 
 // 宣告常數來接收 useRouter() ，方便後續使用
-const router = useRouter();
 
 // 設定路由功能
-function goProductDetail(){
-    router.push('/shop/productdetail')
-};
+
+
+
+
+// 商品
+const products = ref([
+
+]);
 
 </script>
 
@@ -85,8 +90,7 @@ function goProductDetail(){
 
 <!-- 中間商品列表 -->
     <section class="list-all">
-        <TestProductCard 
-        @click="goProductDetail()"/>
+        <TestProductCard />
     </section>
 
 <!-- 下方頁碼 -->
