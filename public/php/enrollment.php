0cd6777bdba7320254fbe05154a020b692e12f56
admin <aassdd80915@gmail.com>
2025-12-26 10:38:37 +0800

    add 可以寄送otp到gmail

--- public/php/enrollment.php
+++ public/php/enrollment.php
@@ -2,6 +2,44 @@
   require_once 'conn.php';
   $member = json_decode(file_get_contents("php://input"), true);
 
+  session_start();
+
+  $checksql = '
+    SELECT COUNT(*) 
+    FROM member 
+    WHERE email = :email
+  ';
+  $checkstmt = $pdo->prepare($checksql);
+  $checkstmt->bindValue(':email', $member['email']);
+  $checkstmt->execute();
+  // fetchColumn 可以只拿單一欄位
+  $count = $checkstmt->fetchColumn();
+
+  if($count > 0){
+    echo json_encode([
+        'success' => false,
+        'message' => 'This email address is already registered.'
+    ]);
+    exit;
+  }
+
+  if(!isset($_SESSION['otp']) || $member['otp'] !== $_SESSION['otp'] || $member['email'] !== $_SESSION['otp_email']){
+    echo json_encode([
+      'success' => false,
+      'message' => 'erification code error or please obtain verification code first.'
+    ]);
+
+    exit;
+  }
+  if(time() - $_SESSION['otp_email'] > 600){
+    echo json_encode([
+      'success' => false,
+      'message' => 'The verification code has expired. Please obtain a new one.'
+    ]);
+
+    exit;
+  }
+
   $sql = '
     INSERT INTO member(email, password, status, role, pointscard, createdate, updatetime)
     VALUES
@@ -13,7 +51,8 @@
     $stmt->bindValue(':password', $member['password']);
     $stmt->execute();
     header('Content-Type: application/json; charset=utf-8');
-
+    unset($_SESSION['otp']);
+    unset($_SESSION['otp_email']);
     echo json_encode(['success' => true]);
 
 
---
+++ public/php/save_otp.php
@@ -0,0 +1,29 @@
+  <?php
+  require_once 'conn.php';
+  $otp = json_decode(file_get_contents("php://input"), true);
+
+
+  session_start();
+
+  $email = $data['email'] ?? '';
+  $otp = $data['otp'] ?? '';
+
+  if(!empty($email) && !empty($otp)){
+    $_SESSION['otp'] = $otp;
+    $_SESSION['otp_email'] = $email;
+    $_SESSION['otp_time'] = time();
+    echo json_encode([
+        'success' => true,
+        'message' => 'OTP save in seesion'
+    ]);    
+  }else{
+    echo json_encode([
+        'success' => false,
+        'message' => 'Invalid data'
+    ]);
+  }
+
+
+
+
+  ?>
