f04516eb401cd4ccb795b0ea107ce5b5be5440eb
irenewhc <wh410601@gmail.com>
2026-01-02 11:58:11 +0800

    add

--- public/php/getProductList.php
+++ public/php/getProductList.php
@@ -1,8 +1,17 @@
 <?php
-//連線
+// 連線
 require_once 'conn.php';
 
-// 
+// SQL
+$sql = "select * 
+        from product p
+	    left join product_detail pd
+        on p.product_ID = pd.product_ID;";
+
+
+
+
+
 
 
 
