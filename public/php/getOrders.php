64996988558f86dad98c544cbf9c666ab0a96755
michael9487 <michael4292015@gmail.com>
2026-01-02 10:51:52 +0800

    add none

--- public/php/getOrders.php
+++ public/php/getOrders.php
@@ -9,11 +9,6 @@
   $stmt->execute();
   $rows = $stmt->fetchAll();
 
-
-    // 告訴瀏覽器是純資料 (JSON)
-    header('Content-Type: application/json; charset=utf-8');
-
-    // 把 PHP 陣列 ($rows) 轉成 JSON 字串並傳到前端
     echo json_encode($rows);
 
   ?>
