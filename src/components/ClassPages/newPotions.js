43698229efcf84a7d6136370ddc92802cff2cf7e
Sophia <sophiafang0219@gmail.com>
2026-01-08 16:46:53 +0800

    modify potion game

---
+++ src/components/ClassPages/newPotions.js
@@ -0,0 +1,159 @@
+export const newPotions = {
+
+    9: {
+      name: 'classes.potiongame.potionName9',
+      resultIntro: 'classes.potiongame.potion9',
+      imgUrl: 'Classes/potions/potion9.png',
+      recipe: {
+        1: {
+          name: 'Aiyu',
+          imgUrl: 'Classes/potions/recipeAiyu.png',
+        },
+        2: {
+          name: 'Winter Melon',
+          imgUrl: 'Classes/potions/recipeEastMelon.png',
+        },
+        3: {
+          name: 'Greentea',
+          imgUrl: 'Classes/potions/recipeGreentea.png',
+        },
+      }
+    },
+    10: {
+      name: 'classes.potiongame.potionName10',
+      resultIntro: 'classes.potiongame.potion10',
+      imgUrl: 'Classes/potions/potion10.png',
+      recipe: {
+        1: {
+          name: 'Lemon',
+          imgUrl: 'Classes/potions/recipeLemon.png',
+        },
+        2: {
+          name: 'Greentea',
+          imgUrl: 'Classes/potions/recipeGreentea.png',
+        },
+        3: {
+          name: 'Coconut',
+          imgUrl: 'Classes/potions/recipeCoconut.png',
+        },
+      }
+    },
+    11: {
+      name: 'classes.potiongame.potionName11',
+      resultIntro: 'classes.potiongame.potion11',
+      imgUrl: 'Classes/potions/potion11.png',
+      recipe: {
+        1: {
+          name: 'milk',
+          imgUrl: 'Classes/potions/recipeMilk.png',
+        },
+        2: {
+          name: 'Xian Tsou',
+          imgUrl: 'Classes/potions/recipeXianTsou.png',
+        },
+        3: {
+          name: 'BlackTea',
+          imgUrl: 'Classes/potions/recipeBlackTea.png',
+        },
+        4: {
+          name: 'suger',
+          imgUrl: 'Classes/potions/recipeSuger.png',
+        },
+      }
+    },
+    12: {
+      name: 'classes.potiongame.potionName12',
+      resultIntro: 'classes.potiongame.potion12',
+      imgUrl: 'Classes/potions/potion12.png',
+      recipe: {
+        1: {
+          name: 'suger',
+          imgUrl: 'Classes/potions/recipeSuger.png',
+        },
+        2: {
+          name: 'Topioca',
+          imgUrl: 'Classes/potions/recipeTopioca.png',
+        },
+        3: {
+          name: 'Greentea',
+          imgUrl: 'Classes/potions/recipeGreentea.png',
+        },
+      }
+    },
+    13: {
+      name: 'classes.potiongame.potionName13',
+      resultIntro: 'classes.potiongame.potion13',
+      imgUrl: 'Classes/potions/potion13.png',
+      recipe: {
+        1: {
+          name: 'Winter Melon',
+          imgUrl: 'Classes/potions/recipeEastMelon.png',
+        },
+        2: {
+          name: 'Lemon',
+          imgUrl: 'Classes/potions/recipeLemon.png',
+        },
+        3: {
+          name: 'Topioca',
+          imgUrl: 'Classes/potions/recipeTopioca.png',
+        },
+      }
+    },
+    14: {
+      name: 'classes.potiongame.potionName14',
+      resultIntro: 'classes.potiongame.potion14',
+      imgUrl: 'Classes/potions/potion14.png',
+      recipe: {
+        1: {
+          name: 'milk',
+          imgUrl: 'Classes/potions/recipeMilk.png',
+        },
+        2: {
+          name: 'Greentea',
+          imgUrl: 'Classes/potions/recipeGreentea.png',
+        },
+        3: {
+          name: 'Coconut',
+          imgUrl: 'Classes/potions/recipeCoconut.png',
+        },
+      }
+    },
+    15: {
+      name: 'classes.potiongame.potionName15',
+      resultIntro: 'classes.potiongame.potion15',
+      imgUrl: 'Classes/potions/potion15.png',
+      recipe: {
+        1: {
+          name: 'Winter Melon',
+          imgUrl: 'Classes/potions/recipeEastMelon.png',
+        },
+        2: {
+          name: 'milk',
+          imgUrl: 'Classes/potions/recipeMilk.png',
+        },
+        3: {
+          name: 'Xian Tsou',
+          imgUrl: 'Classes/potions/recipeXianTsou.png',
+        },
+      }
+    },
+    16: {
+      name: 'classes.potiongame.potionName16',
+      resultIntro: 'classes.potiongame.potion16',
+      imgUrl: 'Classes/potions/potion16.png',
+      recipe: {
+        1: {
+          name: 'Lemon',
+          imgUrl: 'Classes/potions/recipeLemon.png',
+        },
+        2: {
+          name: 'Coconut',
+          imgUrl: 'Classes/potions/recipeCoconut.png',
+        },
+        3: {
+          name: 'BlackTea',
+          imgUrl: 'Classes/potions/recipeBlackTea.png',
+        },
+      }
+    },
+};
